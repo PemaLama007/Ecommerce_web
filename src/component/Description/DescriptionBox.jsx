a7b33f3ba1a16153f6266487e46919e895154db9
PemaLama007 <pema3528@gmail.com>
2024-03-22 21:12:07 +0545

    updated css errors

---
+++ src/component/Description/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './descriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionnox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="description-nav-box-fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos non accusamus ab in voluptatibus sed obcaecati nostrum, corporis blanditiis porro nobis, vitae aliquam? Perspiciatis eligendi repellendus tempora voluptates! Quis quibusdam, sint sapiente, placeat, accusantium tempora quidem aperiam dolor ipsam mollitia illo temporibus repellendus commodi autem vero sunt quaerat pariatur eius?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur laborum minima eius omnis quos sapiente, dolores et impedit. Illum fugit reprehenderit nostrum aperiam consectetur, consequatur voluptatibus enim earum quo quia odit quasi illo veniam quod laborum eius recusandae magnam dignissimos porro consequuntur ab qui aut. Recusandae hic magni, dolorem odio excepturi ipsum harum repellendus aperiam, earum voluptates sed itaque ad, vitae quia sint dignissimos ducimus omnis necessitatibus quos repudiandae vero.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
